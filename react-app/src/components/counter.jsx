f0ece050856e6238baed01fce5fe6bb8138bb27b
Nishanth Reddy <srinishanthreddy@gmail.com>
2018-12-04 20:02:59 -0600

    add class names dynamically

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 0
+    };
     render(){
+        let classes = "badge m-2 badge";
+        classes += (this.state.count === 0) ? "warning" : "primary";
         return(
             <div>
-                <h1>Hello world</h1>
-                <button>Increment</button>
+                <span className="badge badge-primary m-2">{this.formatCounter()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
+}
+
+formatCounter(){
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count; 
 }
 }
 
