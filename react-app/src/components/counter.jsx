5cad50e3b0caedf3f93900a1323056da3c2d87cc
Nishanth Reddy <srinishanthreddy@gmail.com>
2018-12-04 21:47:41 -0600

    add conditional rendering of tags

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,20 +2,33 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: []
     };
+    
     render(){
         return(
             <div>
-                <span className="{this.getBadgeClasses()}">{this.formatCounter()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <div>
+                {this.state.tags.length === 0 && "Please add tags!"}
+                {this.renderTags()}
+                </div>
+                {/* <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul> */}
             </div>
         );
-}
+    }
+
+    renderTags(){
+        if(this.state.tags.length ===0) return <p>There are no tags</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
+        console.log("classes:",classes);
         return classes;
     }
 
