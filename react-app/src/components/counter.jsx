dd4eec296ba6123a02e009b80981b9ade8d1b475
Nishanth Reddy <srinishanthreddy@gmail.com>
2018-12-04 20:19:44 -0600

    correct class name

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -9,7 +9,7 @@ class Counter extends Component {
         classes += (this.state.count === 0) ? "warning" : "primary";
         return(
             <div>
-                <span className="badge badge-primary m-2">{this.formatCounter()}</span>
+                <span className="{classes}">{this.formatCounter()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
