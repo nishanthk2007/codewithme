aa4493da0a3e0a5dd9c4ebc493e4da98dd6e4796
Nishanth Reddy <srinishanthreddy@gmail.com>
2018-12-11 13:16:56 -0600

    multiple components sync and lifting state up

---
+++ react-app/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+
+class NavBar extends Component{
+    state ={};
+    render(){
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand">Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span></a>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
