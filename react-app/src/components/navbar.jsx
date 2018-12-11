179e8918c303c138ad08b96de5d6769e49241a1f
Nishanth Reddy <srinishanthreddy@gmail.com>
2018-12-11 13:35:01 -0600

    nav bar to state less component we pass props as argument and remove this

--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -1,14 +1,11 @@
-import React,{ Component } from 'react';
+//state less component we pass props as argument and remove this
 
-class NavBar extends Component{
-    state ={};
-    render(){
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand">Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span></a>
-            </nav>
+const NavBar = (props) => {
+    return(
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand">Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span></a>
+        </nav>
         );
-    }
 }
 
 export default NavBar;
