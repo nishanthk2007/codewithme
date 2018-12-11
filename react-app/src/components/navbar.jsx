0e040cbd8ecd2b7d30943ac10d728fa5a4708166
Nishanth Reddy <srinishanthreddy@gmail.com>
2018-12-11 13:53:09 -0600

    destructure props argument and use directly instead of props

--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
 //state less component we pass props as argument and remove this
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
     return(
         <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand">Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span></a>
+            <a className="navbar-brand">
+            Navbar {" "}
+            <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+            </span>
+            </a>
         </nav>
         );
 }
